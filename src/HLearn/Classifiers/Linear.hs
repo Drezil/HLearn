0c8aaefb3fb9e63ab8809ab9474bdd0098823b46
Stefan Dresselhaus <sdressel@techfak.uni-bielefeld.de>
2016-08-02 10:31:59 +0200

    added broken svm

--- src/HLearn/Classifiers/Linear.hs
+++ src/HLearn/Classifiers/Linear.hs
@@ -5,6 +5,8 @@ module HLearn.Classifiers.Linear
 import SubHask
 import SubHask.Category.Trans.Derivative
 import SubHask.Compatibility.Containers
+import SubHask.Algebra.Vector
+import SubHask.Algebra.Array
 
 import HLearn.History
 import HLearn.Optimization.Univariate
@@ -13,6 +15,8 @@ import HLearn.Optimization.Multivariate
 import qualified Prelude as P
 import qualified Data.List as L
 import Debug.Trace
+
+import Control.Monad.Trans (liftIO)
 
 --------------------------------------------------------------------------------
 
@@ -34,11 +38,14 @@ type instance Scalar (GLM x y) = Scalar x
 -- \sum_i \alpha_i = 1; w = \sum_i \alpha_i x_i
 
 data SVM x = SVM
-    { alphas :: IntMap' (Scalar x,x, Bool)
+    { alphas :: UVector "datapoints" (Scalar x)
+    , svmdata :: UArray (Labeled' x Bool)
+    , kernel :: x -> x -> Scalar x
     , numa :: Int
     }
 
-deriving instance (Show x, Show (Scalar x)) => Show (SVM x)
+instance (Show x, Show (Scalar x), Show (UVector "datapoints" (Scalar x)), Show (UArray (Labeled' x Bool))) => Show (SVM x) where
+        show (SVM a v k numa) = "SVM { alphas = " ++ show a ++ ", svmdata = " ++ show v ++ ", numa = " ++ show numa ++ " }"
 
 type instance Scalar (SVM x) = Scalar x
 
@@ -118,41 +125,102 @@ trainGLM_ optmethod loss lambda xs = do
 
 --------------------------------------------------------------------------------
 
+
+-- FIXME: Types won't work out ..
 {-# INLINEABLE trainSVM_ #-}
 trainSVM_ :: forall xys x y cxt.
         ( Hilbert x
         , Bounded (Scalar x)
+        , BoundedField (Scalar x)
         , Show (Scalar x)
+        , Scalar x ~ Double
+        , x ~ UVector "dim" (Scalar x)
         ) => (x -> x -> Scalar x)                               -- ^ Kernel
         -> Scalar x                                             -- ^ C-Parameter
         -> Scalar x                                             -- ^ Convergence-difference
         -> Scalar x                                             -- ^ Learning rate
         -> [Labeled' x Bool]                                    -- ^ Dataset with 2 classes
-        -> History cxt (SVM x)
+        -> ( cxt (Iterator_cgd (UVector "datapoints" (Scalar x)))
+           , cxt (Iterator_cgd (Scalar x))
+           , cxt (LineBracket (Scalar x))
+           , cxt (Iterator_brent (Scalar x))
+           ) => History cxt (SVM x)
 trainSVM_ k c crit eta xs = do
-        let init_alphas = fromIxList $ L.zip [0..] $ (\(Labeled' x y) -> (zero,x,y)) P.<$> xs :: IntMap' (Scalar x, x, Bool)
-        alphas' <- untilConvergence crit eta init_alphas
+        let init_alphas = (unsafeToModule $ L.take (length xs) $ L.repeat zero) :: UVector "datapoints" (Scalar x)
+            sdata = fromList xs
+        alphas' <- fmap x1 $ gcd k c sdata init_alphas --untilConvergence c crit eta init_alphas
         return $ SVM
             { alphas = alphas'
-            , numa = length alphas'
+            , svmdata = sdata
+            , kernel = k
+            , numa = length xs
             }
         where
                 -- summed squared error
-                sse :: Bounded (Scalar x) => IntMap' (Scalar x,x,Bool) -> Scalar x
-                sse al = sum $ imap (\_ (Labeled' xj yj) -> pointError xj yj) xs
+                --sse :: (Bounded (Scalar x), IsScalar x)=> Scalar x -> [Labeled' x Bool] -> IntMap' (Scalar x,x,Bool) -> Scalar (Scalar x,x,Bool)
+                -- sse :: (x -> x -> Scalar x) -> Scalar x -> UArray (Labeled' x Bool) -> UVector "datapoints" (Scalar x) -> Scalar x
+                sse k c xys alpha = err --(err + aiequilibrium + regularization) :: Scalar x
                         where
-                                pointError :: Hilbert x => x -> Bool -> Scalar x
-                                pointError xj yj = (\x -> x * x) $ sum $ values $ imap (\_ (ai,xi,_) -> ai * (k xi xj) * bool2num yj) al
-                untilConvergence  crit eta al = untilConvergence' crit eta al maxBound
-                untilConvergence' crit eta al err = beginFunction ("trainSVM("++show err++")") $ do
-                        let al' = imap (opt al) al
-                            err' = sse al'
-                        if err - err' < crit then
-                          return al'
-                        else
-                          untilConvergence' crit eta al' err'
-                opt al _ (ai,xi,yi) = (min c $ max zero $ ai + eta * (one - bool2num yi * (sum $ (\(aj,xj,yj) -> aj * bool2num yj * k xj xi) P.<$> values al)), xi, yi)
+                                -- err :: Scalar x
+                                err = doublemap (\i j -> (a i) * (a j) * (bool2num (y i)) * (bool2num (y j)) * (k (x i) (x j))) alpha
+                                -- aiequilibrium :: Scalar x
+                                aiequilibrium = doublemap (\i j -> (a i) * (a j) * bool2num (y i) * bool2num (y j)) alpha
+                                -- regularization :: Scalar x
+                                regularization = alpha <> alpha / c
+                                doublemap f d = sum $ imap (\ki _ -> sum $ imap (\kj _ -> f ki kj) d) d
+                                -- a :: Integral a => a -> Scalar x
+                                a i = alpha ! (fromIntegral i)
+                                -- y :: Integral a => a -> Bool
+                                y i = yLabeled' $ xys ! (fromIntegral i)
+                                -- x :: Integral a => a -> x
+                                x i = xLabeled' $ xs ! (fromIntegral i)
+                -- sse' :: (x -> x -> Scalar x) -> Scalar x -> UArray (Labeled' x Bool) -> UVector "datapoints" (Scalar x) -> UVector "datapoints" (Scalar x)
+                sse' k c xys alpha = imap (\i _ -> err i + aiequilibrium i + regularization i) alpha
+                        where
+                                -- err :: Integral a => a -> Scalar x
+                                err i = sum $ imap (\j _ -> (a i) * bool2num (y i) * bool2num (y j) * k (x i) (x j)) alpha
+                                -- aiequilibrium :: Integral a => a -> Scalar x
+                                aiequilibrium i = sum $ imap (\j _ -> (a j) * bool2num (y i) * bool2num (y j)) alpha
+                                -- regularization :: Integral a => a -> Scalar x
+                                regularization i = (a i)*(a i) / (2*c)
+                                -- a :: Integral a => a -> Scalar x
+                                a i = alpha ! (fromIntegral i)
+                                -- y :: Integral a => a -> Bool
+                                y i = yLabeled' $ xys ! (fromIntegral i)
+                                -- x :: Integral a => a -> x
+                                x i = xLabeled' $ xys ! (fromIntegral i)
+                -- minFunc :: (x -> x -> Scalar x) -> Scalar x -> UArray (Labeled' x Bool) -> C1 (UVector "datapoints" (Scalar x) -> Scalar x)
+                minFunc k c sdata = unsafeProveC1 f f'
+                    where
+                        -- f :: UVector "datapoints" (Scalar x) -> Scalar x
+                        f  w = sse  k c sdata w
+                        -- f' :: UVector "datapoints" (Scalar x) -> UVector "datapoints" (Scalar x)
+                        f' w = sse' k c sdata w
+                -- gcd :: (x -> x -> Scalar x) -> Scalar x -> UArray (Labeled' x Bool) -> UVector "datapoints" (Scalar x) -> History cxt (Iterator_cgd (UVector "datapoints" (Scalar x)))
+                gcd k c sdata xs = fminunc_cgd_
+                             fletcherReeves
+                             (lineSearch_brent (stop_brent 1e-6 || maxIterations 50 || noProgress || fx1grows))
+                             (mulTolerance 1e-9 || maxIterations 50 || noProgress {- || fx1grows-})
+                             xs
+                             (minFunc k c sdata)
 
+                -- untilConvergence  c crit eta al = untilConvergence' c crit eta al maxBound
+                -- untilConvergence' c crit eta al err = beginFunction ("trainSVM()") $ do
+                --         let da = imap (diff al) al
+                --             al' = imap (opt al da (maximum $ values da)) al
+                --             sa = sum $ (\(a,_,_) -> a) P.<$> values al'
+                --             al'' = trace (show sa) $ imap (\_ (a,x,y) -> (a/sa,x,y)) al'
+                --             err' = sse c al''
+                --         if abs (err' - err) < crit then
+                --             do
+                --                 trace (show $ "Error: " ++ show err ++ " -> " ++show err') $ return al
+                --         else
+                --             do
+                --                 trace (show $ "Error: " ++ show err ++ " -> " ++show err') $ untilConvergence' c crit eta al'' err'
+                -- FIXME: ai with 0 can safely be removed from the map
+                --opt al _ (ai,xi,yi) = (min c $ max zero $ ai + eta * (one - bool2num yi * (sum $ (\(aj,xj,yj) -> aj * bool2num yj * k xj xi) P.<$> values al)), xi, yi)
+                --opt al da etainv key (ai,xi,yi) = (min c $ max zero $ trace (show ai ++ " -> " ++ show (ai + (1/etainv) * da!key)) $ ai + (1/etainv) * da!key, xi, yi)
+                --diff al _ a@(ai,xi,yi) = (-ai / (2*c) - bool2num yi * (sum $ (\(aj,xj,yj) -> aj * bool2num yj * k xj xi + aj * ai) P.<$> L.filter (/= a) (values al)))
 
 
 --------------------------------------------------------------------------------
@@ -176,6 +244,9 @@ validate loss xs model@(GLM ws _) = sum $ map go xs -- /(fromIntegral $ length x
         go xy@(Labeled' x y) = loss y' xy $ ws!y'
             where
                 y' = classify model x
+
+-- decide :: (Hilbert x) => SVM x -> x -> Scalar x
+-- decide (SVM al sv sl k _) x = sum $ (\key -> bool2num (sl!key) * (al!key) * k (sv!key) x) P.<$> indices sv
 
 {-# INLINEABLE loss_01 #-}
 loss_01 :: (HasScalar x, Eq y) => y -> Labeled' x y -> x -> Scalar x
